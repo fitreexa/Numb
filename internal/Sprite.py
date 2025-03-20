cf815f50a32d94aa50b52a4723c75ec72e346cd2
fitreexa <89209562227ib@gmail.com>
2025-03-20 19:10:13 +0300

    сделал прототип

---
+++ internal/Sprite.py
@@ -0,0 +1,14 @@
+import pygame
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self,imaged,x,y,sizex,sizey,speed):
+        super().__init__()
+        self.image=pygame.transform.scale(pygame.image.load(imaged),(sizex,sizey))
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+        self.sizex=sizex
+        self.sizey=sizey
+        self.speed=speed
+    def reset(self):
+        pygame.screen.blit(self.image,(self.rect.x,self.rect.y))
--- internal/main.py
+++ internal/main.py
@@ -1,11 +1,14 @@
 import pygame
 from helper import resource_path
+from Sprite import *
 
 pygame.init()
 
-width,height= 1920,1020
+width,height= 1000,563
 screen=pygame.display.set_mode((width,height))
+background=pygame.image.load(resource_path('assets/field.png'))
 pygame.display.set_caption('future game')
+
 
 player=pygame.image.load(resource_path('assets/КУБ.jpg'))
 player_rect=player.get_rect(center=(width//2,height//2))
@@ -17,20 +20,20 @@ game=True
 FPS=60
 timer=pygame.time.Clock()
 while game:
-    screen.fill((1 ,255 ,30))
+    screen.blit(background,(0,0))
     screen.blit(player,player_rect)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game=False
     keys=pygame.key.get_pressed()
-    if keys[pygame.K_UP]: 
+    if keys[pygame.K_UP] and player_rect.y >1: 
         player_rect.y -= speed
-    if keys[pygame.K_DOWN]: 
+    if keys[pygame.K_DOWN]and player_rect.y < 500: 
         player_rect.y += speed
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT]and player_rect.x >1:
         player_rect.x -= speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT]and player_rect.x <900:
         player_rect.x += speed
 
 pygame.quit()
