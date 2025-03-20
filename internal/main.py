535f699fdbda1ea5c8ce0966f7c7c90a6d01fe26
fitreexa <89209562227ib@gmail.com>
2025-03-20 19:31:04 +0300

    asd

--- internal/main.py
+++ internal/main.py
@@ -1,6 +1,7 @@
 import pygame
 from helper import resource_path
 from Sprite import *
+from class_player import Player
 
 pygame.init()
 
@@ -10,10 +11,11 @@ background=pygame.image.load(resource_path('assets/field.png'))
 pygame.display.set_caption('future game')
 
 
-player=pygame.image.load(resource_path('assets/КУБ.jpg'))
-player_rect=player.get_rect(center=(width//2,height//2))
+# player=pygame.image.load(resource_path('assets/КУБ.jpg'))
+# player_rect=player.get_rect(center=(width//2,height//2))
 
-speed=2
+sprit1=Player(resource_path('assets/КУБ.jpg'),10,400,65,65,10)
+player_rect=sprit1.get_rect(center=(width//2,height//2))
 
 game=True
 
@@ -21,19 +23,19 @@ FPS=60
 timer=pygame.time.Clock()
 while game:
     screen.blit(background,(0,0))
-    screen.blit(player,player_rect)
+    sprit1.reset()
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game=False
     keys=pygame.key.get_pressed()
-    if keys[pygame.K_UP] and player_rect.y >1: 
-        player_rect.y -= speed
-    if keys[pygame.K_DOWN]and player_rect.y < 500: 
-        player_rect.y += speed
-    if keys[pygame.K_LEFT]and player_rect.x >1:
-        player_rect.x -= speed
-    if keys[pygame.K_RIGHT]and player_rect.x <900:
-        player_rect.x += speed
+    if keys[pygame.K_UP] and sprit1.rect.y >1: 
+        sprit1.up()
+    if keys[pygame.K_DOWN]and sprit1.rect.y < 1000: 
+        sprit1.down()
+    if keys[pygame.K_LEFT]and sprit1.rect.x >1:
+        sprit1.left()
+    if keys[pygame.K_RIGHT]and sprit1.rect.x <600:
+        sprit1.right()
 
 pygame.quit()
