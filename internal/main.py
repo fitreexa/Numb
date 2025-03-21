7b1faa768c4ebfba8b968d0b471c00e3fb89a04c
fitreexa <89209562227ib@gmail.com>
2025-03-21 13:43:02 +0300

    создание прыжка

--- internal/main.py
+++ internal/main.py
@@ -15,8 +15,13 @@ player = pygame.image.load(resource_path('assets/ам ням.webp'))
 player_rect=player.get_rect(center=(width//16,height//1.1))
 game=True
 
+player_y=500
+
 vertical_velocity = 0 
 gravity = 1 
+
+jump=False
+jump_size=10
 
 FPS=60
 timer=pygame.time.Clock()
@@ -28,15 +33,20 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game=False
-        if keys[pygame.K_UP]: 
-            if event.key == pygame.K_SPACE: 
-                vertical_velocity = -10
-                vertical_velocity += gravity 
-                if player_rect.y >= 465: 
-                    player_rect.y = 465 
-                    vertical_velocity = 0 
-    vertical_velocity += gravity 
-    player_rect.y += vertical_velocity   
+    if not jump:
+        if keys[pygame.K_UP]:
+            jump =True
+    else:
+        if jump_size >= -10:
+            if jump_size > 0:
+                player_y -= jump_size
+            else:
+                player_y += jump_size
+            jump_size -= 1
+        else:
+            jump=False
+            jump_size = 10
+
     if keys[pygame.K_LEFT]and player_rect.x >1:
         player_rect.x -= 4
     if keys[pygame.K_RIGHT]and player_rect.x < 900:
